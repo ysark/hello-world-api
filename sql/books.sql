11afa56357abe9edc9655a53f90bd93b2db819fb
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-11-12 15:58:16 +0900

    change @Query

--- sql/books.sql
+++ sql/books.sql
@@ -3,9 +3,11 @@ docker exec -it postgres95 psql -U ashiras -d sample_db
 CREATE DATABASE sample_db
     OWNER = ashiras;
 
-CREATE TABLE Books (
-  NAME      varchar(80)
+DROP TABLE books;
+CREATE TABLE books (
+  name      VARCHAR(80),
+  comments  VARCHAR(80)
 );
 
-insert into books values ('Angular');
-insert into books values ('Spring boot');
+insert into books values ('Angular', 'Angular の本');
+insert into books values ('Spring boot', 'Spring boot の本');
